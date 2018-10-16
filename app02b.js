7b01813085939dc59f7061416c512b7e59d747a7
Dennis Allard <allard@oceanpark.com>
2018-10-16 13:57:02 -0700

    added some files

---
+++ app02b.js
@@ -0,0 +1,7 @@
+console.log("ZERO")
+var localTutor=require('./NodeTutorial.js');
+console.log("ONE")
+var tut = new localTutor.NodeTutorial();  // Create and save object
+console.log("TWO")
+tut.pTutor();  // Call function on object
+console.log("THREE")
